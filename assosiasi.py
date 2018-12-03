b507918514899915c28631b2a39ed697e6911f69
dwexid <xolaxote@gmail.com>
2018-12-03 17:50:32 +0700

    update

--- assosiasi.py
+++ assosiasi.py
@@ -2,13 +2,23 @@ import re
 import pyfpgrowth
 import pprint
 
-datas = open("dataset.txt", "r")
+datas = open("data1.txt", "r")
 twits = datas.readlines()
 
 clearedTwits = []
 for twit in twits:
     clearS = re.sub("[^a-zA-Z ]", "", twit)
     clearS = re.sub("  |   ", " ", clearS)
+    clearS = re.sub(" at ", " ", clearS)
+    clearS = re.sub(" and ", " ", clearS)
+    clearS = re.sub("amp", "", clearS)
+    clearS = re.sub("dan", "", clearS)
+    clearS = re.sub("and", "", clearS)
+    clearS = re.sub("with", "", clearS)
+    clearS = re.sub("RT", "", clearS)
+    clearS = re.sub(" yang ", " ", clearS)
+    clearS = re.sub("Yang", "", clearS)
+    clearS = re.sub("With", "", clearS)
     clearedTwits.append(clearS)
 
 dataset = []
@@ -17,7 +27,8 @@ for dt in clearedTwits:
     dataset.append(data)
 
 patterns = pyfpgrowth.find_frequent_patterns(dataset, 2)
-#rules = pyfpgrowth.generate_association_rules(patterns, 0.3)    
+rules = pyfpgrowth.generate_association_rules(patterns, 1)    
 
-print(patterns)
+pprint.pprint(rules)
 datas.close()
+ 
